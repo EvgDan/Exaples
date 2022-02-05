d096f66dfea786229758c894186511ddc6774ac5
Даньшин Евгений <nanabatova@gmail.com>
2022-02-05 18:33:29 +0300

    add example010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array ={1,2,3,46,5, 46,6,7,8};
+int n = array.Length;
+int find = 46;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+      Console.WriteLine(index);  
+      break;
+    }
+   index++; // index = index + 1;
+}
